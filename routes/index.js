51c97860935bf5780868b92f30e1b8504f84b8fb
Tomastomaslol <tnygren@mediaingenunity.com>
2014-10-23 17:15:24 +0100

    added js version of pixel

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,7 @@ router.get('/pixel.gif', function(req, res) {
 	console.log("\n cookie       : ", res.req.headers['cookie'], "\n");
 	console.log("\n referer      : ", res.req.headers['referer'], "\n");
 
-
-	//console.log("request is what : ", req);
-//	console.log("res is what: ", res.req.headers);
-	var gif = new Buffer([
+     var gif = new Buffer([
 	    0x47, 0x49, 0x46, 0x38, 0x39, 0x61, 0x01, 0x00, 0x01, 0x00, 
 	    0x80, 0x00, 0x00, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x2c, 
 	    0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x02, 
@@ -25,20 +22,24 @@ router.get('/pixel.gif', function(req, res) {
 	console.log("\n\n==================================== BYE!! ======================================= \n\n");
 });
 
+router.get('/pixel.js', function(req, res) {
+	console.log("======================= HELLO!! Got request for pixel.js! ======================= \n\n");
+	console.log("\n host         : ", res.req.headers['host'], "\n");
+	console.log("\n user-agent   : ", res.req.headers['user-agent'], "\n");
+	console.log("\n cookie       : ", res.req.headers['cookie'], "\n");
+	console.log("\n referer      : ", res.req.headers['referer'], "\n");
+
+     var js = "(function(){if(typeof console == 'object'){console.log('Hello im a test pixel hosted at http://tm-pixel-debug.herokuapp.com/pixel.js for debuging stuff');}})();";
+	 
+	res.writeHead(200, {'Content-Type': 'application/x-javascript' });
+	res.end(js);
+	console.log("\n\n==================================== BYE!! ======================================= \n\n");
+});
 module.exports = router;
 
-/*
 
- headers:
-  { host: 'localhost:3000'
-    connection: 'keep-aliv
-    pragma: 'no-cache',
-    'cache-control': 'no-c
-    accept: 'image/webp,
-    'user-agent': 'Mozilla
- Version/7.0 Mobile/11A465
-    referer: 'http://www.t
-    'accept-encoding': 'gz
-    'accept-language': 'en
-    cookie: 'PermanentId=2
+
+
+/*
+Content-Type:application/x-javascript
 */
